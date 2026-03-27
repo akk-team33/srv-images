dd8eb99cf809d88ce304d31851eae3cb04eb0fe6
Andi Kluge <akk-dev@team33.de>
2026-03-27 12:34:34 +0100

    "Hello World" Implementation

---
+++ template-fat/src/main/java/de/team33/service/images/main/HelloController.java
@@ -0,0 +1,13 @@
+package de.team33.service.images.main;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
---
+++ template-fat/src/main/java/de/team33/service/images/main/ImageServiceApplication.java
@@ -0,0 +1,12 @@
+package de.team33.service.images.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageServiceApplication.class, args);
+    }
+}
